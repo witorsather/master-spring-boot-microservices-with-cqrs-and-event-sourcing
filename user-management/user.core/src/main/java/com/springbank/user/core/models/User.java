1a648c8e4ae0b9dd2d0121f54a8d4da9ecf842ed
Witor Sather <satherws@gmail.com>
2023-11-22 08:13:41 -0300

    Implementa User, Account e Role.

--- user-management/user.core/src/main/java/com/springbank/user/core/models/User.java
+++ user-management/user.core/src/main/java/com/springbank/user/core/models/User.java
@@ -4,13 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Document
+@Document(collection = "users")
 public class User {
-    
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private Account account;
 }
